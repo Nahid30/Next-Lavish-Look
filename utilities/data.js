d783d740702733d5dce32441c8ef73e5d5d70f43
Nahid30 <nahidshuvo30@gmail.com>
2022-07-04 15:24:37 +0600

    create some shirt data for my Ecommerce website and also added some images to the images folder

---
+++ utilities/data.js
@@ -0,0 +1,61 @@
+const data = {
+    products : [
+        {
+            name: 'White Shirt',
+            category: 'Shirt',
+            image: '/public/images/shirt1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A Beautiful shirt for every season.'
+        },
+        {
+            name: 'Slim Shirt',
+            category: 'Shirt',
+            image: '/public/images/shirt2.jpg',
+            price: 60,
+            brand: 'Adidas',
+            rating: 4.2,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A Beautiful shirt for winter season.'
+        },
+        {
+            name: 'Casual Shirt',
+            category: 'Shirt',
+            image: '/public/images/shirt3.jpg',
+            price: 50,
+            brand: 'Calvin Klein',
+            rating: 4.3,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A Beautiful shirt for summer season.'
+        },
+        {
+            name: 'Classic Shirt',
+            category: 'Shirt',
+            image: '/public/images/shirt4.jpg',
+            price: 40,
+            brand: 'Hugo Boss',
+            rating: 4.7,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A Beautiful shirt for corporate.'
+        },
+        {
+            name: 'Casual Shirt',
+            category: 'Shirt',
+            image: '/public/images/shirt5.jpg',
+            price: 50,
+            brand: 'Guess',
+            rating: 4.8,
+            numReviews: 10,
+            countInStock: 20,
+            description: 'A Beautiful shirt for every season.'
+        },
+    ],
+};
+
+export default data;
